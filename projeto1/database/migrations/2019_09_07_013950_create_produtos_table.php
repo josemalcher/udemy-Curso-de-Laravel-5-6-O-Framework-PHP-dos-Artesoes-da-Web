9b044006c48e8f54ca9cbbe350d3f3d876d3d385
José Stélio R. Malcher Junior <contato@josemalcher.net>
2019-09-06 22:50:43 -0300

    16 - Criando o Model e o arquivo de Migration

---
+++ projeto1/database/migrations/2019_09_07_013950_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sku');
+            $table->string('titulo');
+            $table->longText('descricao');
+            $table->float('preco',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
